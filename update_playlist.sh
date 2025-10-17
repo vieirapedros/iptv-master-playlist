4d4620be575eb7c4ec72ab063971ec3f4241e721
vieirapedros <76785190+vieirapedros@users.noreply.github.com>
2025-10-17 11:49:27 -0300

    Create update_playlist.sh

---
+++ update_playlist.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# =====================================
+# Script: update_playlist.sh
+# Unifica, limpa e atualiza playlists IPTV
+# =====================================
+
+# Caminho da playlist final
+OUTPUT="master.m3u"
+
+# Lista de fontes
+URLS=(
+  "https://www.apsattv.com/brlg.m3u"
+  "https://www.apsattv.com/ptlg.m3u"
+  "https://www.apsattv.com/arlg.m3u"
+  "https://www.apsattv.com/mxlg.m3u"
+  "https://www.apsattv.com/pelg.m3u"
+  "https://www.apsattv.com/eslg.m3u"
+  "https://www.apsattv.com/uslg.m3u"
+  "https://www.apsattv.com/gblg.m3u"
+  "https://www.apsattv.com/itlg.m3u"
+  "https://www.apsattv.com/frlg.m3u"
+  "https://www.apsattv.com/jplg.m3u"
+  "https://www.apsattv.com/xiaomi.m3u"
+  "https://www.apsattv.com/tclbr.m3u"
+  "https://www.apsattv.com/tcl.m3u"
+  "https://www.apsattv.com/tclplus.m3u"
+  "https://www.apsattv.com/ssungbra.m3u"
+  "https://www.apsattv.com/ssungpor.m3u"
+  "https://www.apsattv.com/ssungmex.m3u"
+  "https://www.apsattv.com/redbox.m3u"
+  "https://www.apsattv.com/rok.m3u"
+  "https://www.apsattv.com/vidaa.m3u"
+  "https://www.apsattv.com/localnow.m3u"
+  "https://www.apsattv.com/moviearkbr.m3u"
+  "https://www.apsattv.com/firetv.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/My-Streams/refs/heads/main/Backup.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/buddylive/refs/heads/main/buddylive_v1.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/My-Streams/refs/heads/main/TheTVApp.m3u8"
+  "https://raw.githubusercontent.com/BuddyChewChew/xumo-playlist-generator/refs/heads/main/playlists/xumo_playlist.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/ppv/refs/heads/main/PPVLand.m3u8"
+  "https://raw.githubusercontent.com/BuddyChewChew/My-Streams/refs/heads/main/StreamedSU.m3u8"
+  "https://raw.githubusercontent.com/BuddyChewChew/My-Streams/refs/heads/main/24-7.m3u8"
+  "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/refs/heads/main/playlists/roku_all.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/refs/heads/main/playlists/plex_all.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/refs/heads/main/playlists/plutotv_all.m3u"
+  "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/refs/heads/main/playlists/samsungtvplus_all.m3u"
+)
+
+# Apaga o arquivo anterior
+rm -f "$OUTPUT"
+
+# Cabeçalho M3U
+echo "#EXTM3U" > "$OUTPUT"
+
+# Baixa todas as listas e concatena
+for url in "${URLS[@]}"; do
+  echo "Baixando $url ..."
+  curl -s "$url" | grep -E "^#EXTINF|^http" >> "$OUTPUT"
+done
+
+# Remove canais duplicados (mesmo link)
+awk '!x[$0]++' "$OUTPUT" > temp.m3u && mv temp.m3u "$OUTPUT"
+
+# Exibe contagem final
+echo "Playlist unificada gerada: $(wc -l < "$OUTPUT") linhas"
